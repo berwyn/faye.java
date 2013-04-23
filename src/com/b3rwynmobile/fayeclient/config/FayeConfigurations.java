3dac01f311b19f191464544281d6803dac74f9b7
Ademar Alves de Oliveira <ademar111190@gmail.com>
2013-04-23 18:27:56 -0300

    Configuration class to facilitate the library usage

---
+++ src/com/b3rwynmobile/fayeclient/config/FayeConfigurations.java
@@ -0,0 +1,18 @@
+package com.b3rwynmobile.fayeclient.config;
+
+/**
+ * @author Ademar Alves de Oliveira
+ * @Apr 23, 2013
+ * @email ademar111190@gmail.com
+ */
+public class FayeConfigurations {
+
+	/**
+	 * Singleton access
+	 */
+	public static FayeConfigurations shared	= new FayeConfigurations();
+
+	public boolean logEnabled = false;
+	public String logTag = "FayeAndroid";
+
+}
