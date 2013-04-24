69e0a0355d8d137459eb035c1c5eeda46d96adf4
Ademar Alves de Oliveira <ademar111190@gmail.com>
2013-04-24 15:43:17 -0300

    a configuration example

--- demo/src/com/b3rwynmobile/fayeclient/demo/MyFayeConfigurations.java
+++ demo/src/com/b3rwynmobile/fayeclient/demo/MyFayeConfigurations.java
@@ -4,18 +4,33 @@ import com.b3rwynmobile.fayeclient.config.FayeConfigurations;
 
 public class MyFayeConfigurations extends FayeConfigurations {
 
-    /**
-     * Need override to work
-     */
-    public String FAYE_URL = "ws://your.url:80/mount";
-    public String FAYE_INITIAL_CHANNEL = "/your_channel";
-    public String FAYE_AUTH_TOKEN = "";
+    public MyFayeConfigurations() {
 
-    /**
-     * Override to debug
-     */
-    public boolean logEnabled = true;
-    public boolean logMethodTrackerEnabled = true;
-    public boolean logExceptionsEnabled = true;
+	/*
+	 * Need override to work, here you put your url initial channel and if
+	 * need the auth token
+	 */
+	FAYE_URL = "ws://example.com:00/mount";
+	FAYE_INITIAL_CHANNEL = "/some_channel";
+	FAYE_AUTH_TOKEN = "";
+
+	/*
+	 * Override to debug, it will show faye logs with logcat tag
+	 * "FayeAndroid" default is false
+	 */
+	logEnabled = true;
+
+	/*
+	 * Override to see faye method called with logcat tag
+	 * "FayeAndroidMethodTracer" default is false
+	 */
+	logMethodTrackerEnabled = true;
+
+	/*
+	 * Override to see faye exceptions use the logcat tag
+	 * "FayeAndroidException" default is false
+	 */
+	logExceptionsEnabled = true;
+    }
 
 }
